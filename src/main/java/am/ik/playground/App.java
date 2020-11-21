f92645e93448abbb96a101b1beb229448be9a0e3
Toshiaki Maki <tmaki@pivotal.io>
2020-11-21 21:12:46 +0900

    * Convert to Spring Boot project * Use Java 11 * Build a native image

--- src/main/java/am/ik/playground/App.java
+++ src/main/java/am/ik/playground/App.java
@@ -1,38 +1,20 @@
 package am.ik.playground;
 
-import java.time.Duration;
-import java.util.Optional;
-
-import org.slf4j.LoggerFactory;
-import reactor.netty.http.server.HttpServer;
-
-import org.springframework.http.server.reactive.HttpHandler;
-import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
-import org.springframework.web.reactive.function.server.HandlerStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+@SpringBootApplication(proxyBeanMethods = false)
 public class App {
 
-	static RouterFunction<ServerResponse> routes() {
+	@Bean
+	public RouterFunction<ServerResponse> routes() {
 		return new BlackholeHandler().routes();
 	}
 
 	public static void main(String[] args) throws Exception {
-		long begin = System.currentTimeMillis();
-		int port = Optional.ofNullable(System.getenv("PORT")) //
-				.map(Integer::parseInt) //
-				.orElse(8080);
-		HttpServer httpServer = HttpServer.create().host("0.0.0.0").port(port);
-		httpServer.route(routes -> {
-			HttpHandler httpHandler = RouterFunctions.toHttpHandler(
-					App.routes(), HandlerStrategies.builder().build());
-			routes.route(x -> true, new ReactorHttpHandlerAdapter(httpHandler));
-		}).bindUntilJavaShutdown(Duration.ofSeconds(3), disposableServer -> {
-			long elapsed = System.currentTimeMillis() - begin;
-			LoggerFactory.getLogger(App.class).info("Started in {} seconds",
-					elapsed / 1000.0);
-		});
+		SpringApplication.run(App.class, args);
 	}
 }
