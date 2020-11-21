a0b460b8504073782d173a6d0e52c182267392ca
Toshiaki Maki <tmaki@pivotal.io>
2020-11-21 19:54:00 +0900

    Bump Spring Boot version

--- src/main/java/am/ik/playground/BlackholeHandler.java
+++ src/main/java/am/ik/playground/BlackholeHandler.java
@@ -2,15 +2,14 @@ package am.ik.playground;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.reactive.function.server.RequestPredicate;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.accepted;
@@ -18,20 +17,20 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 
 public class BlackholeHandler {
 
-    private static final Logger log = LoggerFactory.getLogger(BlackholeHandler.class);
+	private static final Logger log = LoggerFactory.getLogger(BlackholeHandler.class);
 
-    public RouterFunction<ServerResponse> routes() {
-        return route(__ -> true, this::accept);
-    }
+	public RouterFunction<ServerResponse> routes() {
+		return route(__ -> true, this::accept);
+	}
 
-    Mono<ServerResponse> accept(ServerRequest req) {
-        ServerWebExchange exchange = req.exchange();
-        ServerHttpRequest request = exchange.getRequest();
-        return req.bodyToMono(String.class) //
-            .switchIfEmpty(Mono.just("")) //
-            .doOnNext(s -> {
-                log.info("{}\t{}\t{}", request.getMethod(), request.getPath(), s);
-            }) //
-            .flatMap(__ -> ((request.getMethod() == HttpMethod.GET || request.getMethod() == HttpMethod.HEAD) ? ok() : accepted()).build());
-    }
+	Mono<ServerResponse> accept(ServerRequest req) {
+		ServerWebExchange exchange = req.exchange();
+		ServerHttpRequest request = exchange.getRequest();
+		return req.bodyToMono(String.class) //
+				.switchIfEmpty(Mono.just("")) //
+				.doOnNext(s -> {
+					log.info("{}\t{}\t{}", request.getMethod(), request.getPath(), s);
+				}) //
+				.flatMap(__ -> ((request.getMethod() == HttpMethod.GET || request.getMethod() == HttpMethod.HEAD) ? ok() : accepted()).build());
+	}
 }
--- src/test/java/am/ik/playground/HelloHandlerTest.java
+++ src/test/java/am/ik/playground/HelloHandlerTest.java
@@ -1,54 +1,55 @@
 package am.ik.playground;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 public class HelloHandlerTest {
 
-    private WebTestClient testClient;
+	private WebTestClient testClient;
 
-    @Before
-    public void setUp() throws Exception {
-        this.testClient = WebTestClient.bindToRouterFunction(new BlackholeHandler().routes()).build();
-    }
+	@BeforeEach
+	public void setUp() throws Exception {
+		this.testClient = WebTestClient.bindToRouterFunction(new BlackholeHandler().routes()).build();
+	}
 
-    @Test
-    public void get() throws Exception {
-        this.testClient.get()
-            .uri("/foo") //
-            .exchange() //
-            .expectStatus().isOk();
-    }
+	@Test
+	void get() throws Exception {
+		this.testClient.get()
+				.uri("/foo") //
+				.exchange() //
+				.expectStatus().isOk();
+	}
 
 
-    @Test
-    public void post() throws Exception {
-        this.testClient.post() //
-            .uri("/v1/metrics") //
-            .syncBody("{\n" +
-                "        \"applications\": [\n" +
-                "        {\n" +
-                "            \"id\": \"[app_guid]\",\n" +
-                "            \"instances\": [\n" +
-                "            {\n" +
-                "                \"id\": \"[instance_guid]\",\n" +
-                "                \"index\": \"[index]\",\n" +
-                "                \"metrics\": [\n" +
-                "                {\n" +
-                "                    \"name\": \"[metric_name]\",\n" +
-                "                    \"type\": \"gauge\",\n" +
-                "                    \"value\": [metric_value],\n" +
-                "                    \"timestamp\": [timestamp],\n" +
-                "                    \"unit\": \"[metric_unit]\",\n" +
-                "                    \"tags\": {\n" +
-                "                        \"[tag_key]\": \"[tag_value]\"\n" +
-                "                    }\n" +
-                "                }]\n" +
-                "            }]\n" +
-                "        }]\n" +
-                "      }") //
-            .exchange() //
-            .expectStatus().isAccepted();
-    }
+	@Test
+	void post() throws Exception {
+		this.testClient.post() //
+				.uri("/v1/metrics") //
+				.bodyValue("{\n" +
+						"        \"applications\": [\n" +
+						"        {\n" +
+						"            \"id\": \"[app_guid]\",\n" +
+						"            \"instances\": [\n" +
+						"            {\n" +
+						"                \"id\": \"[instance_guid]\",\n" +
+						"                \"index\": \"[index]\",\n" +
+						"                \"metrics\": [\n" +
+						"                {\n" +
+						"                    \"name\": \"[metric_name]\",\n" +
+						"                    \"type\": \"gauge\",\n" +
+						"                    \"value\": [metric_value],\n" +
+						"                    \"timestamp\": [timestamp],\n" +
+						"                    \"unit\": \"[metric_unit]\",\n" +
+						"                    \"tags\": {\n" +
+						"                        \"[tag_key]\": \"[tag_value]\"\n" +
+						"                    }\n" +
+						"                }]\n" +
+						"            }]\n" +
+						"        }]\n" +
+						"      }") //
+				.exchange() //
+				.expectStatus().isAccepted();
+	}
 }
